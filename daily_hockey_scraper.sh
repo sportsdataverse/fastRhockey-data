e0dfa5a1258d063eb45e34aece9874c66d105204
saiemgilani <saiem.gilani@gmail.com>
2021-12-14 12:29:56 -0500

    PHF and NHL Play-by-play and Schedules update

---
+++ daily_hockey_scraper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git pull
+git add .
+"C:\Program Files\R\R-4.1.2\bin\Rscript.exe" phf_daily_scrape.R
+"C:\Program Files\R\R-4.1.2\bin\Rscript.exe" nhl_daily_scrape.R
+git add .
+git commit -m "PHF and NHL Play-by-play and Schedules update" || echo "No changes to commit"
+git pull
+git push
