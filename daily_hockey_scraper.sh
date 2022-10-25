58f5f5e77e47bdd5ced7e28b194a7fade16fe3c7
saiemgilani <saiem.gilani@gmail.com>
2022-10-25 12:55:26 +0000

    PHF and NHL Play-by-play and Schedules update

--- daily_hockey_scraper.sh
+++ daily_hockey_scraper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git pull
 git add .
-R phf_daily_scrape.R
-R nhl_daily_scrape.R
+Rscript phf_daily_scrape.R
+Rscript nhl_daily_scrape.R
 git add .
 git commit -m "PHF and NHL Play-by-play and Schedules update" || echo "No changes to commit"
 git pull
